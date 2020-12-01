d928884cd94cfe01972c467332912a65254c1ee8
Dominic Cabitac <dominic.cabitac@uoit.net>
2020-12-01 08:22:28 -0500

    Fixed font styling for windows laptops

--- footer.php
+++ footer.php
@@ -15,14 +15,14 @@ wp_footer(); ?>
 <footer id="footer" class=" d-lg-block">
 		<div class="container-fluid">
 			<div class="row foot justify-content-between">
-				<div class="col-12 col-xl-5 col-lg-5 offset-md-1 col-xs-4 align-items-start">
+				<div class="col-12 col-xl-5 col-lg-5 col-xs-4 align-items-start">
 					© Verify 2020 | All Rights Restricted
 					<div class="privacy">
 						<a href='<?php echo get_site_url()?>/privacy-policy'>Privacy Policy</a>
 					</div>
 				</div>
 				<div class='col-12 col-xl-5 col-lg-5 FC align-text-end'>
-					<a href='https://www.ferrazcreative.com' target="_blank">REVAMPED™ by Ferraz Creative</a>
+					<a href='https://revampedweb.com/' target="_blank">REVAMPED™ by Ferraz Creative</a>
 				</div>
 			</div>
 		</div>
